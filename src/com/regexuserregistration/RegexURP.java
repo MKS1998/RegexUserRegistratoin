16521329f317d26200b54664a43d0bc781b88307
MKS1998 <nksayyad2900@outlook.com>
2021-10-14 15:17:02 +0530

    Added:User Need To Enter Valid First Name

---
+++ src/com/regexuserregistration/RegexURP.java
@@ -0,0 +1,10 @@
+package com.regexuserregistration;
+
+public class RegexURP {
+	public static void main(String[] args) {
+		System.out.println("..........Welcome to the User Registration Problem.........");
+		UserOperator user = new UserOperator();
+		user.firstName("Mohin");
+
+	}
+}
