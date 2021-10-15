57c7149c5fac48e16234dc9a98d09fb006a8a00f
MKS1998 <nksayyad2900@outlook.com>
2021-10-15 16:26:09 +0530

    Added : Program For The Check Whether Last Name Is Valid Or Not

--- src/com/regexuserregistration/RegexURP.java
+++ src/com/regexuserregistration/RegexURP.java
@@ -1,10 +1,10 @@
 package com.regexuserregistration;
 
 public class RegexURP {
-	public static void main(String[] args) {
-		System.out.println("..........Welcome to the User Registration Problem.........");
-		UserOperator user = new UserOperator();
-		user.firstName("Mohin");
-
-	}
+    public static void main(String[] args) {
+        System.out.println("Welcome to the User Registration Problem using Regex!!");
+        UserOperator user = new UserOperator();
+        user.firstName("Rajendra");
+        user.lastName("Gund");
+    }
 }
--- src/com/regexuserregistration/UserOperator.java
+++ src/com/regexuserregistration/UserOperator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class UserOperator {
 	public void firstName(String firstName) {
-		String regex = "^[A-Z]{1}[a-z]{4,}$";
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(firstName);
 		boolean valid = matcher.matches();
@@ -13,6 +13,18 @@ public class UserOperator {
 			System.out.println("Your name " + firstName + " is Valid");
 		} else {
 			System.out.println("Your name " + firstName + " is Invalid");
+		}
+	}
+
+	public void lastName(String lastName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
+		boolean valid = matcher.matches();
+		if (valid == true) {
+			System.out.println("Your Last name " + lastName + " is Valid");
+		} else {
+			System.out.println("Your Last name " + lastName + " is Invalid");
 		}
 	}
 }
