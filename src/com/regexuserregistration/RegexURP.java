d6744fb5f821d6120e12559a0a338da2baf99415
MKS1998 <nksayyad2900@outlook.com>
2021-10-15 16:50:19 +0530

    Added : PassWord Should Have Minimum 8 Char

--- src/com/regexuserregistration/RegexURP.java
+++ src/com/regexuserregistration/RegexURP.java
@@ -8,5 +8,6 @@ public class RegexURP {
         user.lastName("Sayyad");
         user.emailaddress("mohin@gmail.com");
         user.phoneNo("91 9604315270");
+        user.passRule1("123asASD");
     }
 }
