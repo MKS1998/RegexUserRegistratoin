7a4b4d7cc0b65ab5a98b2879662aa81a1a78e8c9
MKS1998 <nksayyad2900@outlook.com>
2021-10-15 16:41:50 +0530

    Added : Program For The Check Whether Phone Number Is VAlid Or Not

--- src/com/regexuserregistration/RegexURP.java
+++ src/com/regexuserregistration/RegexURP.java
@@ -1,11 +1,12 @@
 package com.regexuserregistration;
 
 public class RegexURP {
-	public static void main(String[] args) {
-		System.out.println(".............Welcome to the User Registration Problem.......");
-		UserOperator user = new UserOperator();
-		user.firstName("Mohin");
-		user.lastName("Sayyad");
-		user.emailaddress("mohin@gmail.com");
-	}
+    public static void main(String[] args) {
+        System.out.println(".......Welcome to the User Registration Problem.........");
+        UserOperator user = new UserOperator();
+        user.firstName("Mohin");
+        user.lastName("Sayyad");
+        user.emailaddress("mohin@gmail.com");
+        user.phoneNo("91 9604315270");
+    }
 }
--- src/com/regexuserregistration/UserOperator.java
+++ src/com/regexuserregistration/UserOperator.java
@@ -28,16 +28,27 @@ public class UserOperator {
 		}
 	}
 
-	public void emailaddress(String emailaddress){
-        String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(emailaddress);
-        boolean valid=matcher.matches();
-        if (valid==true){
-            System.out.println("Your Email Address "+emailaddress+" is Valid");
-        }
-        else{
-            System.out.println("Your Email Address "+emailaddress+" is Invalid");
-        }
-    }
+	public void emailaddress(String emailaddress) {
+		String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(emailaddress);
+		boolean valid = matcher.matches();
+		if (valid == true) {
+			System.out.println("Your Email Address " + emailaddress + " is Valid");
+		} else {
+			System.out.println("Your Email Address " + emailaddress + " is Invalid");
+		}
+	}
+
+	public void phoneNo(String phoneNo) {
+		String regex = "^[0-9]{2} [6-9]{1}[0-9]{9}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(phoneNo);
+		boolean valid = matcher.matches();
+		if (valid == true) {
+			System.out.println("Your Phone Number " + phoneNo + " is Valid");
+		} else {
+			System.out.println("Your Phone Number " + phoneNo + " is Invalid");
+		}
+	}
 }
