d3839d4c85cf71615bf999e82da73cbf17f30b62
MKS1998 <nksayyad2900@outlook.com>
2021-10-17 15:46:46 +0530

    Added : PassWord Should Have Atleast 1 Upper Case

--- src/com/regexuserregistration/RegexURP.java
+++ src/com/regexuserregistration/RegexURP.java
@@ -1,13 +1,13 @@
 package com.regexuserregistration;
 
 public class RegexURP {
-    public static void main(String[] args) {
-        System.out.println(".......Welcome to the User Registration Problem.........");
-        UserOperator user = new UserOperator();
-        user.firstName("Mohin");
-        user.lastName("Sayyad");
-        user.emailaddress("mohin@gmail.com");
-        user.phoneNo("91 9604315270");
-        user.passRule1("123asASD");
-    }
+	public static void main(String[] args) {
+		System.out.println("Welcome to the User Registration Problem using Regex!!");
+		UserOperator user = new UserOperator();
+		user.firstName("Mohin");
+		user.lastName("Sayyad");
+		user.emailaddress("mohin@gmail.com");
+		user.phoneNo("91 7498656661");
+		user.passward("Mohin1234");
+	}
 }
