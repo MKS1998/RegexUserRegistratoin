040f9d67d570f23b177c63eeedd96fb06569d053
MKS1998 <nksayyad2900@outlook.com>
2021-10-17 15:56:56 +0530

    Added : Password Should Have AtLeast Numeric Number in Pass

--- src/com/regexuserregistration/RegexURP.java
+++ src/com/regexuserregistration/RegexURP.java
@@ -2,12 +2,12 @@ package com.regexuserregistration;
 
 public class RegexURP {
 	public static void main(String[] args) {
-		System.out.println("Welcome to the User Registration Problem using Regex!!");
+		System.out.println("Welcome to the User Registration Problem");
 		UserOperator user = new UserOperator();
 		user.firstName("Mohin");
 		user.lastName("Sayyad");
 		user.emailaddress("mohin@gmail.com");
 		user.phoneNo("91 7498656661");
-		user.passward("Mohin1234");
+		user.passward("Mohin1234gg");
 	}
 }
